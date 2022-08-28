d930a0175e847bcb665cbe4a30acb5545bac3c97
OctagonDeluge <junterz675@gmail.com>
2022-08-28 11:22:56 +0400

    branch for heroku deployment

--- src/main/java/ru/tink/practice/controller/TestController.java
+++ src/main/java/ru/tink/practice/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import ru.tink.practice.dto.external.moex.SecuritiesShortInfoDTO;
 
 import java.net.URI;
 
@@ -27,5 +28,18 @@ public class TestController {
                 .queryParam("apikey", "Q9GGE2JFISLWXXWO")
                 .build().toUri();
         return restTemplate.getForEntity(destUrl, String.class).getBody();
+    }
+
+    @GetMapping("/moex")
+    public String testMoex() {
+        URI destUrl = UriComponentsBuilder.fromHttpUrl("https://iss.moex.com/iss")
+                .pathSegment("securities")
+                .pathSegment("SBER").pathSegment(".json")
+                .queryParam("iss.meta", "off")
+                .queryParam("iss.json", "extended")
+                .queryParam("iss.only", "description")
+                .queryParam("description.columns", "name,value")
+                .build().toUri();
+        return restTemplate.getForObject(destUrl, String.class);
     }
 }
