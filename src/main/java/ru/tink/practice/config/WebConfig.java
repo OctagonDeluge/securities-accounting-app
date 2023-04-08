047acbb816766f8ba763f504f2c6e58c6bc3ab85
OctagonDeluge <junterz675@gmail.com>
2023-04-08 12:57:57 +0400

    configured cors

--- src/main/java/ru/tink/practice/config/WebConfig.java
+++ src/main/java/ru/tink/practice/config/WebConfig.java
@@ -1,13 +1,27 @@
 package ru.tink.practice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${client.url}")
+    private String clientUrl;
     @Bean
     public RestTemplate createRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(clientUrl)
+                .allowedHeaders("Authorization")
+                .exposedHeaders("Authorization");
     }
 }
