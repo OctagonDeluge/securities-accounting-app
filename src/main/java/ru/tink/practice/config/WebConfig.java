7708fde8cc71d3b7fff1fa3b01e6dec80ab87d92
OctagonDeluge <junterz675@gmail.com>
2023-04-10 14:20:07 +0400

    editing cors

--- src/main/java/ru/tink/practice/config/WebConfig.java
+++ src/main/java/ru/tink/practice/config/WebConfig.java
@@ -3,6 +3,8 @@ package ru.tink.practice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,8 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(clientUrl)
-                .allowedHeaders("Authorization")
-                .exposedHeaders("Authorization");
+                .allowedHeaders(HttpHeaders.AUTHORIZATION,
+                        HttpHeaders.CONTENT_TYPE,
+                        HttpHeaders.ACCEPT)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
+                .exposedHeaders(HttpHeaders.AUTHORIZATION)
+                .allowedOrigins(clientUrl);
     }
 }
