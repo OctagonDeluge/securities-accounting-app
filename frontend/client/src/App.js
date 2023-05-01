554bff61b873d293ff5b09b9c521fb1ec0505df1
OctagonDeluge <junterz675@gmail.com>
2023-05-01 15:40:17 +0400

    test

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -5,25 +5,41 @@ import {Security} from "./pages/Security";
 import {Exchange} from "./pages/Exchange";
 import {ExchangeSecurityInfo} from "./pages/ExchangeSecurityInfo";
 import {HeaderMiddle} from "./components/headers/HeaderMiddle";
-import React from "react";
+import React, {useState} from "react";
 import {links} from "./constants/HeaderLinks";
 import {PortfolioSecurityInfo} from "./pages/PortfolioSecurityInfo";
 import {Statistics} from "./pages/Statistics";
 import {NotFound} from "./pages/NotFound";
+import {Signup} from "./pages/Signup";
+import {Signin} from "./pages/Signin";
+import {useWalletService} from "./api/service/WalletService";
+import {useAccountService} from "./api/service/AccountService";
 
 export function App() {
+    const walletService = useWalletService();
+    const accountService = useAccountService();
     return (
         <div className="App">
-            <HeaderMiddle links={links}/>
-            <Routes>
-                <Route path="/portfolio" element={<Portfolio/>} />
-                <Route path="/portfolio/:portfolioId/exchange" element={<Exchange/>} />
-                <Route path="/portfolio/:portfolioId/security" element={<Security/>} />
-                <Route path="/portfolio/:portfolioId/statistics" element={<Statistics/>} />
-                <Route path="/portfolio/:portfolioId/security/:securityId" element={<PortfolioSecurityInfo/>} />
-                <Route path="/portfolio/:portfolioId/exchange/:exchange/security/:secid" element={<ExchangeSecurityInfo/>} />
-                <Route path="*" element={<NotFound/>} />
-            </Routes>
+            {accountService.auth ?
+                <>
+                    <HeaderMiddle service={walletService} links={links}/>
+                    <Routes>
+                        <Route path="/portfolio" element={<Portfolio/>} />
+                        <Route path="/portfolio/:portfolioId/exchange" element={<Exchange/>} />
+                        <Route path="/portfolio/:portfolioId/security" element={<Security/>} />
+                        <Route path="/portfolio/:portfolioId/statistics" element={<Statistics/>} />
+                        <Route path="/portfolio/:portfolioId/security/:securityId" element={<PortfolioSecurityInfo/>} />
+                        <Route path="/portfolio/:portfolioId/exchange/:exchange/security/:secid" element={<ExchangeSecurityInfo/>} />
+                    </Routes>
+                </>
+                :
+                <Routes>
+                    <Route path="/signup" element={<Signup/>} />
+                    <Route path="/signin" element={<Signin props={accountService}/>} />
+                    <Route path="*" element={<NotFound/>} />
+                </Routes>
+            }
+
         </div>
     );
 }
