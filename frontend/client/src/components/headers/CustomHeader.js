24c90e3e293030c2e5c3cd7115d357d03eb9c751
OctagonDeluge <ojenkins0zerog@gmail.com>
2024-08-03 15:31:11 +0400

    refactoring

--- frontend/client/src/components/headers/CustomHeader.js
+++ frontend/client/src/components/headers/CustomHeader.js
@@ -1,15 +1,13 @@
-import {Anchor, Burger, createStyles} from "@mantine/core";
-import {useEffect, useState} from "react";
+import {Anchor} from "@mantine/core";
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {Wallet} from "../elements/Wallet";
 import {AvatarMenu} from "../elements/AvatarMenu";
 import "../../assets/styles/CustomHeaderStyles.css"
-import {useMediaQuery} from "react-responsive";
 
 export function CustomHeader({links, service, service1}) {
     const [active, setActive] = useState(links[0].link);
     const navigate = useNavigate();
-    const isMobile = useMediaQuery({ query: '(max-width: 600px)' })
 
     const items = links.map((link) => (
         <Anchor
@@ -28,14 +26,9 @@ export function CustomHeader({links, service, service1}) {
 
     return (
         <div className="header">
-            {
-                isMobile ?
-                    <Burger opened/>
-                    :
-                    <div className="tabs">
-                        {items}
-                    </div>
-            }
+            <div className="tabs">
+                {items}
+            </div>
             <div className="info">
                 <Wallet state={service.wallet}/>
                 <AvatarMenu service={service1}/>
